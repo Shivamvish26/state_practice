759a6b005a552c28d15445863f79f0ea6fdf847a
Shubham Vishwakarma <127945668+Shivamvish26@users.noreply.github.com>
2025-06-10 11:13:16 +0530

    added new branch

--- state_practice/src/App.jsx
+++ state_practice/src/App.jsx
@@ -1,14 +1,14 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
+      {/* Started with react state practice */}
       <div>
         <h1>Hello World</h1>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
